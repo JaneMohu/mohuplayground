1eaaf05b185bf291a989b49761309b6c7d4a5e98
Jane Kelly <jane@studiomohu.com>
2012-11-15 17:24:20 +0000

    Main html/css template

---
+++ js/main.js
@@ -0,0 +1,48 @@
+(function() {
+  var html_editor = document.querySelector('#html textarea'),
+    css_editor = document.querySelector('#css textarea'),
+    js_editor = document.querySelector('#js textarea');
+
+  var editors = [html_editor, css_editor, js_editor];
+
+  // Attaching the onkeyup event
+  editors.forEach(function(editor, i, arr) {
+    editor.addEventListener('keyup', function() {
+      render();
+    }, false);
+  });
+
+  var base_tpl =
+    "<!doctype html>\n" +
+    "<html>\n\t" +
+    "<head>\n\t\t" +
+    "<meta charset=\"utf-8\">\n\t\t" +
+    "<title>Test</title>\n\n\t\t\n\t" +
+    "</head>\n\t" +
+    "<body>\n\t\n\t" +
+    "</body>\n" +
+    "</html>";
+
+  var prepareSource = function() {
+    var html = html_editor.value,
+        css = css_editor.value,
+        js = js_editor.value,
+        src = '';
+
+    // HTML
+    // Replace closing body tag with html plus /body
+    src = base_tpl.replace('</body>', html + '</body');
+
+    // CSS
+    css = '<style>' + css + '</style>';
+    // Replace closing head tag with css plus /head
+    src = src.replace('</head>', css + '</head>');
+
+    // Javascript
+    js = '<script>' + js + '</script>';
+    src = src.replace('</body>', js + '</body>');
+
+    return src;
+  };
+
+}());
