fc3486f8088c5c786c8136173b12d5af0eaefa77
Jane Kelly <jane@studiomohu.com>
2012-11-16 15:24:22 +0000

    updates to codemirror

--- js/main.js
+++ js/main.js
@@ -1,20 +1,4 @@
 (function() {
-  var html_editor = document.querySelector('#html textarea'),
-    css_editor = document.querySelector('#css textarea'),
-    js_editor = document.querySelector('#js textarea');
-
-  var editors = [html_editor, css_editor, js_editor],
-      code = '';
-
-  // Attaching the onkeyup event
-  editors.forEach(function(editor, i, arr) {
-    editor.addEventListener('keyup', function() {
-      if(code !== this.value) {
-        render();
-        code = this.value;
-      }
-    }, false);
-  });
 
   var base_tpl =
     "<!doctype html>\n" +
@@ -31,9 +15,9 @@
   // (HTML5 postMessage works in IE8 and up)
 
   var prepareSource = function() {
-    var html = html_editor.value,
-        css = css_editor.value,
-        js = js_editor.value,
+    var html = html_editor.getValue(),
+        css = css_editor.getValue(),
+        js = js_editor.getValue(),
         src = '';
 
     // HTML
@@ -60,6 +44,65 @@
     iframe_doc.open();
     iframe_doc.write(source);
     iframe_doc.close();
+  }
+
+  // CM Options
+  var cm_opt = {
+    mode: 'text/html',
+    gutter: true,
+    lineNumbers: true,
+
+    onChange: function (instance, changes) {
+      render();
+    }
+  };
+
+  // HTML EDITOR
+  var html_box = document.querySelector('#html textarea');
+  var html_editor = CodeMirror.fromTextArea(html_box, cm_opt);
+
+  // CSS EDITOR
+  cm_opt.mode = 'css';
+  var css_box = document.querySelector('#css textarea'),
+      css_editor = CodeMirror.fromTextArea(css_box, cm_opt);
+
+  // JAVASCRIPT EDITOR
+  cm_opt.mode = 'javascript';
+  var js_box = document.querySelector('#js textarea'),
+      js_editor = CodeMirror.fromTextArea(js_box, cm_opt);
+
+  // SETTING CODE EDITORS INITIAL CONTENT
+  html_editor.setValue('Hello World');
+  css_editor.setValue('body { color: red; }');
+  
+  // RENDER CALL ON PAGE LOAD
+  // NOT NEEDED ANYMORE, SINCE WE RELY
+  // ON CODEMIRROR'S onChange OPTION THAT GETS
+  // TRIGGERED ON setValue
+  // render();
+  
+  
+  // NOT SO IMPORTANT - IF YOU NEED TO DO THIS
+  // THEN THIS SHOULD GO TO CSS
+  
+  /*
+    Fixing the Height of CodeMirror.
+    You might want to do this in CSS instead
+    of JS and override the styles from the main
+    codemirror.css
+  */
+  var cms = document.querySelectorAll('.CodeMirror');
+  for (var i = 0; i < cms.length; i++) {
+    
+    cms[i].style.position = 'absolute';
+    cms[i].style.top = '30px';
+    cms[i].style.bottom = '0';
+    cms[i].style.left = '0';
+    cms[i].style.right = '0';
+  }
+  var cms = document.querySelectorAll('.CodeMirror-scroll');
+  for (var i = 0; i < cms.length; i++) {
+    cms[i].style.height = '100%';
   }
 
 }());
